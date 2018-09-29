ffdcc6363cc677ef9d00bedfcb7a2fe30e942bbb
GonzaloHirsch <hirschgonzalo@yahoo.com.ar>
2018-09-29 12:43:01 -0300

    Termine el script del player

---
+++ Scimus Nihil Game/Assets/_Scripts/bulletController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour {
+
+    [HideInInspector]
+    public float bulletLife = 2.0f;
+    public bool canMove = false;
+    public float bulletSpeed = 2.0f;
+    [HideInInspector]
+    public float existanceTime;
+    [HideInInspector]
+    public Vector2 direction;
+    [HideInInspector]
+    public Rigidbody2D bulletBody;
+    [HideInInspector]
+    public Transform bulletTransform;
+
+    private player1Controller playerController;
+
+
+    void Start (){
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<player1Controller>();
+        bulletBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate (){
+        if (canMove){
+            existanceTime += Time.deltaTime;
+            BulletMovement();
+            if (existanceTime >= bulletLife){
+                canMove = false;
+                playerController.StoreBullet(this.gameObject);
+            }
+        }
+	}
+
+    void BulletMovement(){
+        bulletBody.position += direction * Time.deltaTime * bulletSpeed; 
+    }
+}
--- Scimus Nihil Game/Assets/_Scripts/player1Controller.cs
+++ Scimus Nihil Game/Assets/_Scripts/player1Controller.cs
@@ -5,21 +5,96 @@ using UnityEngine;
 public class player1Controller : MonoBehaviour {
 
     public float playerSpeed = 10f;
+
     public GameObject bullet;
+    public int initialGunAmmo = 10;
+    public int queueGunAmmo = 4;
+    public float bulletLife = 2.0f;
+    public float bulletSpeed = 2.0f;
+
+    [HideInInspector]
+    public bool isAlive = false;
 
     private Vector2 moveDirection;
+    private Vector2 bulletDirection;
     private Rigidbody2D playerRB;
+    private Queue<GameObject> gunQueue;
+    private int gunAmmo;
+    private readonly float EPSILON = 0.000000000001f;
 
-	void Start () {
+    void Start () {
+        bulletDirection = Vector2.up;
         playerRB = GetComponent<Rigidbody2D>();
+        gunAmmo = initialGunAmmo;
+
+        FillGun();
 	}
 	
 	void FixedUpdate () {
         Walk();
+        Shoot();
 	}
 
     void Walk(){
-        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        float horizInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+        moveDirection = new Vector2(horizInput, vertInput);
+
         playerRB.position += moveDirection * Time.deltaTime * playerSpeed;
+
+        if (System.Math.Abs(horizInput) > EPSILON || System.Math.Abs(vertInput) > EPSILON)
+            bulletDirection = GetDirection(horizInput);
+    }
+
+    void Shoot(){
+        if (Input.GetKeyDown(KeyCode.RightShift) && initialGunAmmo > 0){
+            ShootBullet();
+        }
+    }
+
+    //Si queremos agregar para arriba agregamos el float del axis
+    Vector2 GetDirection(float horizAxis){
+        Vector2 direction = new Vector2();
+
+        if (horizAxis > 0)
+            direction = Vector2.right;
+        else if (horizAxis < 0)
+            direction = Vector2.left;
+        /*
+        else if (vertAxis > 0)
+            direction = Vector2.up;
+        else if (vertAxis < 0)
+            direction = Vector2.down;
+            */
+
+        return direction;
+    }
+
+    void FillGun(){
+        gunQueue = new Queue<GameObject>();
+        GameObject bulletInstance;
+        for (int i = 0; i < queueGunAmmo; i++){
+            bulletInstance = Instantiate(bullet);
+            bulletInstance.SetActive(false);
+            gunQueue.Enqueue(bulletInstance);
+        }
+    }
+
+    void ShootBullet(){
+        initialGunAmmo--;
+        GameObject bulletInstance = gunQueue.Dequeue();
+        bulletController controller = bulletInstance.GetComponent<bulletController>();
+
+        bulletInstance.SetActive(true);
+        controller.canMove = true;
+        controller.direction = bulletDirection;
+        bulletInstance.transform.position = playerRB.position;
+        controller.existanceTime = 0f;
+        controller.bulletLife = bulletLife;
+    }
+
+    public void StoreBullet(GameObject bulletInstance){
+        bulletInstance.SetActive(false);
+        gunQueue.Enqueue(bulletInstance);
     }
 }
