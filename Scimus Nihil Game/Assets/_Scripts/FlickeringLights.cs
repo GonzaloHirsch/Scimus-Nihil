2e0b038ea1e6835bc71c9b0e4cde76bcf02f6cb1
GonzaloHirsch <hirschgonzalo@yahoo.com.ar>
2018-09-30 10:14:16 -0300

    Fonts and others

---
+++ Scimus Nihil Game/Assets/_Scripts/FlickeringLights.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FlickeringLights : MonoBehaviour {
+
+    public Light[] lights;
+    [Range(0, 100)]
+    public int chanceOfFlicker = 70;
+
+    private int lightIndex;
+	
+	void Update () {
+        if (Random.Range(0, 101) <= chanceOfFlicker)
+            Flicker();
+	}
+
+    void Flicker(){
+        lightIndex = Random.Range(0, lights.Length);
+        Light myLight = lights[lightIndex];
+        DOTween.To(
+            getter: () => { return myLight.intensity; },
+            setter: (float value) => { myLight.intensity = value; },
+            endValue: 0f,
+            duration: 0.5f).OnComplete(() => DOTween.To(
+            getter: () => { return myLight.intensity; },
+            setter: (float value) => { myLight.intensity = value; },
+                endValue: Random.Range(4.5f, 6.5f),
+            duration: 0.5f));
+    }
+}
